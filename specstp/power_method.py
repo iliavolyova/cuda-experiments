43b9c89550307e9f8278a4bc0577412f9b5bbff4
iliavolyova <neuromancer999@gmail.com>
2015-05-07 17:14:18 +0200

    cublas memory, python generator and checker

---
+++ specstp/power_method.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def power_method(A, x0, maxit):
+    y = x0
+    for i in xrange(maxit):
+        y = A*y
+        y = y/np.linalg.norm(y)
+    return y
+
+def check(mat, power_eigvec):
+
+    prd = mat*power_eigvec
+    eigval = prd[0]/power_eigvec[0]
+    print 'computed eigenvalue :' , eigval
+    [eigs, vecs] = np.linalg.eig(mat)
+    abseigs = list(abs(eigs))
+    ind = abseigs.index(max(abseigs))
+    print ' largest eigenvalue :', eigs[ind]
+
+def main():
+    dim = input("Dimenzije matrice:")
+
+    arr = np.fromfile('A_' + dim + '.dat', dtype=np.float64)
+    A = arr.reshape(dim,dim)
+    xarr = np.fromfile('x0_' + dim + '.dat', dtype=np.float64)
+    x0 = xarr.reshape(dim, 1)
+
+    print A, x0
+
+    maxiter = input('Broj iteracija:')
+    rndmat = np.matrix(A)
+    rndvec = np.matrix(x0)
+    eigmax = power_method(rndmat, rndvec, maxiter)
+    check(rndmat, eigmax)
+
+main()
--- specstp/specstp.cu
+++ specstp/specstp.cu
@@ -3,7 +3,7 @@
 #include    "cuda_auxiliary.h"
 
 /*
- * compile: nvcc specstp.cu -o specstp
+ * compile: nvcc specstp.cu -lcublas -o specstp
  * trebaju ti dgemv, dnrm2
  */
 
@@ -11,13 +11,60 @@
 int main(int argc, char **argv)
 {
     cublasHandle_t cublas_handle;
-    cublas_exec(cublasCreate(&cublas_handle));
 
-    int version;
-    cublas_exec(cublasGetVersion(cublas_handle, &version));
-    printf("\nUsing CuBlas version: %d", version);
+    FILE *fp_A = NULL;
+    FILE *fp_x = NULL;
+
+    double *hst_A = NULL;
+    double *hst_x = NULL;
+
+    double *dev_A = NULL;
+    double *dev_x = NULL;
+
+    double ONE = 1.0;
+
+    int dim;
+    int steps;
+
+    if (argc != 5) {
+        fprintf(stderr, "usage: %s N A.dat\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    dim = atoi(argv[1]);
+    steps = atoi(argv[4]);
+
+    open_file(fp_A, argv[2], "r");
+    host_alloc(hst_A, double, dim * dim);
+
+    open_file(fp_x, argv[3], "r");
+    host_alloc(hst_x, double, dim);
+
+    cuda_exec(cudaHostRegister(hst_A, dim * dim * sizeof(double), cudaHostRegisterDefault));
+    read_file(hst_A, sizeof(double), dim * dim, fp_A);
+
+    cuda_exec(cudaHostRegister(hst_x, dim * sizeof(double), cudaHostRegisterDefault));
+    read_file(hst_A, sizeof(double), dim, fp_x);
+
+    cublas_exec(cublasCreate(&cublas_handle));
+    cublas_exec(cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_DEVICE));
+
+    cublas_exec(cublasSetMatrix(dim, dim, sizeof(double), hst_A, dim, dev_A, dim));
+    cublas_exec(cublasSetVector(dim, sizeof(double), hst_x, ONE, dev_x, ONE));
+
+    int i;
+    for (i = 0; i < steps; i++){
+
+    }
+
+    printf("done");
 
     cublas_exec(cublasDestroy(cublas_handle));
+    cudaFree(dev_A);
+    cudaFree(dev_x);
+
+    host_free(hst_A);
+    host_free(hst_x);
 
     return 0;
 }
