e8fa6d594068048562eaf8d745b532c9b2ac7f0a
iliavolyova <neuromancer999@gmail.com>
2015-05-07 20:00:06 +0200

    fix

--- specstp/specstp.cu
+++ specstp/specstp.cu
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
 
         cublas_exec(cublasDgemv(cublas_handle, CUBLAS_OP_T, dim, dim, &ONE, dev_A, dim, dev_x, 1, &ONE, dev_x, 1));
     }
-    cublas_exec(cublasDdot(cublas_handle, dim, dev_x, dev_y, &eigval));
+    cublas_exec(cublasDdot(cublas_handle, dim, dev_x, dim, dev_y, 1, &eigval));
 
     printf("\nSpectrum: %#.16lg\n", eigval);
 
