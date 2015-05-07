23e4558ffd693a0f36973b39cf090db02a9aa602
iliavolyova <neuromancer999@gmail.com>
2015-05-08 01:43:36 +0200

    test dgemv

--- specstp/speceps.cu
+++ specstp/speceps.cu
@@ -149,12 +149,13 @@ int main(int argc, char **argv)
         gpu_dnrm2<<<grid_size, block_size>>>(dev_y, dev_nrm_inv, dim);
         gpu_dscal<<<grid_size, block_size>>>(dev_y, dev_x, dev_nrm_inv, dim);
         gpu_dgemv<<<grid_size, block_size>>>(dev_A, dev_x, dev_y, dim);
-        gpu_ddot<<<grid_size, block_size>>>(dev_x, dev_y, dev_lambda, dim);
+        #gpu_ddot<<<grid_size, block_size>>>(dev_x, dev_y, dev_lambda, dim);
 
-        gpu_dscal<<<grid_size, block_size>>>(dev_x, dev_x, dev_lambda, dim);
+        #gpu_dscal<<<grid_size, block_size>>>(dev_x, dev_x, dev_lambda, dim);
         gpu_subtract<<<grid_size, block_size>>>(dev_y, dev_x, dev_x, dim);
         gpu_dnrm2<<<grid_size, block_size>>>(dev_x, dev_nrm_inv, dim);
 
+        converged = true;
         if (dev_nrm_inv[0] < EPS * dev_lambda[0])
             converged = true;
     }
